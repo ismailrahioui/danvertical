7f1ffccbb9f6047861dc94bfe9caf901a5c2b221
ismailrahioui <ismail.Orahiouy@gmail.com>
2024-03-24 17:58:38 +0000

    main

--- danvertical/danvertical/settings.py
+++ danvertical/danvertical/settings.py
@@ -56,6 +56,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build' ,'static')
+STATIC_ROOT = os.path.join(BASE_DIR ,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT=BASE_DIR /' media'
 
